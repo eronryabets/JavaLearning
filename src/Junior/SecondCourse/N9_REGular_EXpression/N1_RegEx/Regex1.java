7aca90094d2cb7aa92222f7c4350c500b7bd7aa1
Ihor Ryabets <eronryabets@gmail.com>
2022-01-08 22:13:36 +0200

    update

--- src/Junior/SecondCourse/N9_REGular_EXpression/N1_RegEx/Regex1.java
+++ src/Junior/SecondCourse/N9_REGular_EXpression/N1_RegEx/Regex1.java
@@ -1,4 +1,13 @@
 package Junior.SecondCourse.N9_REGular_EXpression.N1_RegEx;
 
 public class Regex1 {
+    public static void main(String[] args) {
+        String s =
+                "Ivanov Vasiliy, Russia, Moscow, Lenin street, 51, Flat 48," +
+                " email: vivanov@mail.ru, Postcode: AA99, PhoneNumber: +123456789;"
+                + "Petrova Maria, Ukraine, Kyiv, Lomonosov street, 33, Flat 18," +
+                " email: masha@yandex.ru, Postcode: UKR54, PhoneNumber: +987654321;"
+                +"Chuck Norris, USA, Hollywood, All stars street, 87, Flat21, " +
+                "email: chuck@gmail.com, Postcode: USA23, Phone Number: +136478952.";
+    }
 }
