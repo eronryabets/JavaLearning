98d5b0be925de32061f8df100909ce502f302865
eronryabets <eronryabets@gmail.com>
2022-06-04 12:11:10 +0300

    Update Test2Arrays.java

--- src/Junior/FirstCourse/HomeWork/HW18_Arrays/Test2Arrays.java
+++ src/Junior/FirstCourse/HomeWork/HW18_Arrays/Test2Arrays.java
@@ -3,6 +3,40 @@ package Junior.FirstCourse.HomeWork.HW18_Arrays;
 
 public class Test2Arrays {
 
+    public static void arraysOnDisplay(int[] array) {
+        for (int i : array) {
+            System.out.println(i);
+        }
+    }
+
+    //для двумерных массивов с разной величиной
+    public static void arraysOnDisplay(String[][] array) {
+        for(int i = 0; i < array.length; i++){
+            for(int j = 0; j < array[i].length; j++){
+                System.out.println(array[i][j] + " = ["+i+"]" + "["+j+"]" );
+            }
+        }
+    }
+
+    //по заданию, с красивым выводом
+    public static void arraysOnDisplayHW(String[][] array) {
+        System.out.print("{ ");
+        for(int i = 0; i < array.length; i++){
+            System.out.print("{ ");
+            for(int j = 0; j < array[i].length; j++){
+                System.out.print(array[i][j]);
+                if(j != (array[i].length-1)){ // если не последний элемент в массиве ставь "," !=0[2]
+                    System.out.print(", ");
+                }
+            }
+            System.out.print(" }");
+            if(i != (array[i].length-1)){ //если это не последний вложенный масив, ставь ","
+                System.out.print(", ");
+            }
+        }
+        System.out.print(" }");
+    }
+    
     public static void main(String[] args) {
         String[][] s1 = new String[2][4];
 
